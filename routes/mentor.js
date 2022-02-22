ff9b348b1eb8d6322f301ac49deb761fd689f2ab
Darshan Raut <56134448+darshanraut123@users.noreply.github.com>
2022-02-22 22:39:50 +0530

    Add files via upload

---
+++ routes/mentor.js
@@ -0,0 +1,95 @@
+var express = require('express');
+const res = require('express/lib/response');
+var mentorRouter = express.Router();
+require("dotenv").config();
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+//Base get route to list all mentors data
+mentorRouter.get('/',async (req,res)=>{
+
+  let client =null;
+  try {
+      client = await MongoClient.connect(process.env.mongouri);
+      const db = await client.db('student-mentor');
+      let document = await db.collection('mentor').find().toArray();
+
+      res.json({
+        message:"Data fetched Successfully!",
+        data:document
+      })
+
+    } catch (error) { 
+      console.log(error);
+      res.json({
+        message:"Internal Server Error!"
+      })
+    }
+    finally{
+      client.close();
+    }
+
+}); 
+
+//Post route to ladd an new mentor
+mentorRouter.post('/addMentor',async (req,res)=>{
+
+    let client =null;
+    try {
+      client = await MongoClient.connect(process.env.mongouri);
+      const db = await client.db('student-mentor');
+      let data = await db.collection("mentor").find().toArray();
+      if(data.filter(res=>res._id===req.body._id).length>=1)
+        res.json({message:"duplicate entry"});
+        else{
+      let doc = await db.collection('mentor').insertOne(req.body);
+
+      res.json({
+        message:"Data Added Successfully!",
+        data:doc
+      })
+        }
+
+    } catch (error) { 
+      console.log(error);
+      res.json({
+        message:"Internal Server Error!"
+      })
+    }
+    finally{
+      client.close();
+    }
+
+}); 
+
+//Route to show all students under specific mentor
+mentorRouter.get("/students/:id",async (req,res)=>{
+    let client = null;
+    try{
+      client = await MongoClient.connect(process.env.mongouri);
+      const db = await client.db("student-mentor");
+        let document = await db.collection('mentor').aggregate([
+            {$match:{_id:`${req.params.id}`}}
+            ,{ $lookup:
+               {
+                 from: 'student',
+                 localField: 'students',
+                 foreignField: '_id',
+                 as: 'students'
+               }
+             }
+            ]).toArray();
+
+        res.json({"Students under mentor": document[0].students});
+    }
+    catch(error){
+        console.log(error);
+        res.json({"message":"Server error"})
+    }
+    finally{client.close()}
+})
+
+//Exporting mentors router
+module.exports = mentorRouter;
+
+
---
+++ routes/student.js
@@ -0,0 +1,127 @@
+const { response } = require("express");
+var express = require("express");
+var studentRouter = express.Router();
+require("dotenv").config();
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+//get route to show all student data
+studentRouter.get("/", async (req, res) => {
+  let client = null;
+  try {
+    client = await MongoClient.connect(process.env.mongouri);
+    const db = await client.db("student-mentor");
+    let document = await db.collection("student").find().toArray();
+
+    res.json({
+      message: "Data fetched Successfully!",
+      data: document,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Internal Server Error!",
+    });
+  } finally {
+    client.close();
+  }
+});
+
+//Post request to create new student
+studentRouter.post("/addStudent", async (req, res) => {
+  let client =null;
+  try {
+    client = await MongoClient.connect(process.env.mongouri);
+    const db = await client.db("student-mentor");
+    let data = await db.collection("student").find().toArray();
+    if(data.filter(res=>res._id===req.body._id).length>=1)
+    res.json({message:"duplicate entry"});
+    else{
+      let doc = await db.collection("student").insertOne(req.body);
+      res.json({
+      message: "Data Added Successfully!",
+      data: doc,
+    });
+  }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Internal Server Error!",
+    });
+  } finally {
+    client.close();
+  }
+});
+
+//Put request to change the mentor of the student, if student is not yet assigned to any mentor still it works
+studentRouter.put("/changementor", async (req, res) => {
+  let client =null;
+  try {
+    client = await MongoClient.connect(process.env.mongouri);
+    const db = await client.db("student-mentor");
+    let oldone = await db
+      .collection("mentor")
+      .findOne({ students: req.body.studentid });
+    //checks if mentor is already assigned or not. If its already assigned then its mentor is removed
+    if (oldone != null) {
+      await db
+        .collection("mentor")
+        .updateOne(
+          { _id: oldone._id },
+          { $pull: { students: req.body.studentid } }
+        );
+    }
+    let isStd = await db.collection("student").distinct("_id");
+
+    //new mentor is assigned here if student is present in student collection ,
+    // elsewise create student first meassage shown
+    if (isStd.includes(req.body.studentid)) {
+      let isdone = await db
+        .collection("mentor")
+        .updateOne(
+          { _id: req.body.changetomentor },
+          { $push: { students: req.body.studentid } }
+        );
+      res.json({ res: isdone });
+    }
+    else
+      res.json({
+      res: "The student with mentioned id is not present in student collection, please create a new student first",
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({ res: err });
+  } finally {
+    client.close();
+  }
+});
+
+//All students to which the mentor is not yet assigned
+studentRouter.get("/nomentorstudents", async (req, res) => {
+  let client =null;
+  try {
+    client = await MongoClient.connect(process.env.mongouri);
+    const db = await client.db("student-mentor");
+    let assignedStudents = await db.collection("mentor").distinct("students");
+    let allStudents = await db.collection("student").distinct("_id");
+    //Extracting non assigned mentor students
+    let nonAssigned = allStudents.filter(stud=>!assignedStudents.includes(stud));
+    let students = await db.collection("student").find().toArray();
+    let non = students.filter(res=>nonAssigned.includes(res._id));
+    console.log()
+    //Sending the response
+    res.json({
+      message: "Data fetched Successfully!",
+      "students without mentor": non
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Internal Server Error!",
+    });
+  } finally {
+    client.close();
+  }
+});
+
+module.exports = studentRouter;
